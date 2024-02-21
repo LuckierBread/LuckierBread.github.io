3023f38ec3997b1e906db5b606674b9e18944fd0
NeuralConnectionsAI <michael.ursu@outlook.com>
2024-02-21 11:44:29 -0700

    changed Logo to text display

--- components/Header.js
+++ components/Header.js
@@ -14,13 +14,14 @@ const Header = () => {
         <div className='flex flex-col lg:flex-row justify-between items-center gap-y-6 py-8'>
           {/* logo */}
           <Link href={'/'}>
-            <Image
+            {/* <Image
               src={'/logo.svg'}
               width={220}
               height={48}
               alt=''
               priority={true}
-            />
+            /> */}
+            <a>Michael Ursuliak<a>
           </Link>
           {/* socials */}
           <Socials />

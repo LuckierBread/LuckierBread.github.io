876b512d12e9144bb0f056ca99a3ce28b5365b8d
NeuralConnectionsAI <michael.ursu@outlook.com>
2024-03-05 18:17:37 -0700

    Changed Link structure to include "a" wrapper

--- components/Header.js
+++ components/Header.js
@@ -14,14 +14,9 @@ const Header = () => {
         <div className='flex flex-col lg:flex-row justify-between items-center gap-y-6 py-8'>
           {/* logo */}
           <Link href={'/'}>
-            {/* <Image
-              src={'/logo.svg'}
-              width={220}
-              height={48}
-              alt=''
-              priority={true}
-            /> */}
-            <a style={{fontSize:'24px'}}><span style={{fontWeight:'bold'}}>Michael</span> Ursuliak</a>
+            <a style={{fontSize:'24px'}}>
+              <span style={{fontWeight:'bold'}}>Michael</span> Ursuliak
+            </a>
           </Link>
           {/* socials */}
           <Socials />
--- components/Socials.js
+++ components/Socials.js
@@ -3,24 +3,26 @@ import Link from 'next/link';
 
 // icons
 import {
-  RiYoutubeLine,
-  RiInstagramLine,
-  RiFacebookLine,
-  RiDribbbleLine,
-  RiBehanceLine,
-  RiPinterestLine,
   RiLinkedinBoxFill,
   RiGithubFill,
 } from 'react-icons/ri';
 
 const Socials = () => {
   return (
-    <div className='flex items-center gap-x-5 text-lg'>
-      <Link href={''} className='hover:text-accent transition-all duration-300'>
-        <RiLinkedinBoxFill />
-      </Link>
-    </div>
+    <>
+      <div className='flex items-center gap-x-5 text-lg'>
+        <a href={'https://www.linkedin.com/in/michael-ursuliak-647175128'} className='hover:text-accent transition-all duration-300'>
+          <RiLinkedinBoxFill />
+        </a>
+      </div>
+
+      <div className='flex items-center gap-x-5 text-lg'>
+        <a href='https://github.com/LuckierBread' className='hover:text-accent transition-all duration-300'>
+          <RiGithubFill />
+        </a>
+      </div>
+    </>
   );
-};
+}
 
 export default Socials;

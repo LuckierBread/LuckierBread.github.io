3b62b2599c90206f44abcf659f634aa1f9ca6624
Michael Ursuliak <luckierbread@gmail.com>
2024-06-04 23:19:05 -0700

    fixed links in work menu

--- components/WorkSlider.js
+++ components/WorkSlider.js
@@ -6,18 +6,22 @@ export const workSlider = {
         {
           title: 'YOLOv9 Car Counter',
           path: '/Car_Demo.PNG',
+          link: 'https://github.com/LuckierBread/YOLOv9-Car-Counter'
         },
         {
           title: 'Neural Netrork Learning Visualization',
           path: '/Importance_Map.png',
+          link: 'https://github.com/LuckierBread/Neural-Network-Learning-Visualization'
         },
         {
           title: 'title',
           path: '/thumb3.jpg',
+          link: ''
         },
         {
           title: 'title',
           path: '/thumb4.jpg',
+          link: ''
         },
       ],
     },
@@ -76,32 +80,36 @@ const WorkSlider = () => {
             <div className='grid grid-cols-2 grid-rows-2 gap-4 cursor-pointer'>
               {slide.images.map((image, index) => {
                 return (
-                  <div
-                    className='relative rounded-lg overflow-hidden flex items-center justify-center group'
-                    key={index}
-                  >
-                    <div className='flex items-center justify-center relative overflow-hidden group'>
-                      {/* image */}
-                      <Image src={image.path} width={500} height={300} alt='' />
-                      {/* overlay gradient */}
-                      <div className='absolute inset-0 bg-gradient-to-l from-transparent via-[#e838cc] to-[#4a22bd] opacity-0 group-hover:opacity-80 transition-all duration-700'></div>
-                      {/* title */}
-                      <div className='absolute bottom-0 translate-y-full group-hover:-translate-y-10 group-hover:xl:-translate-y-20 transition-all duration-300'>
-                        <div className='flex items-center gap-x-2 text-[13px] tracking-[0.2em]'>
-                          {/* title part 1 */}
-                          <div className='delay-100'>GitHub</div>
-                          {/* title part 2 */}
-                          <div className='translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-150'>
-                            Repo
-                          </div>
-                          {/* icon */}
-                          <div className='text-xl translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-200'>
-                            <BsArrowRight />
+                  <a href={image.link} key = {index}>
+
+                    <div
+                      className='relative rounded-lg overflow-hidden flex items-center justify-center group'
+                      key={index}
+                    >
+                     <div className='flex items-center justify-center relative overflow-hidden group'>
+                        {/* image */}
+                        <Image src={image.path} width={500} height={300} alt='' />
+                        {/* overlay gradient */}
+                        <div className='absolute inset-0 bg-gradient-to-l from-transparent via-[#e838cc] to-[#4a22bd] opacity-0 group-hover:opacity-80 transition-all duration-700'></div>
+                        {/* title */}
+                        <div className='absolute bottom-0 translate-y-full group-hover:-translate-y-10 group-hover:xl:-translate-y-20 transition-all duration-300'>
+                          <div className='flex items-center gap-x-2 text-[13px] tracking-[0.2em]'>
+                            {/* title part 1 */}
+                            <div className='delay-100'>GitHub</div>
+                            {/* title part 2 */}
+                            <div className='translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-150'>
+                              Repo
+                            </div>
+                            {/* icon */}
+                            <div className='text-xl translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-200'>
+                              <BsArrowRight />
+                            </div>
                           </div>
                         </div>
+                        
+                        </div>
                       </div>
-                    </div>
-                  </div>
+                    </a>
                 );
               })}
             </div>
